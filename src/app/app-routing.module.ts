9c96a9a947152a669b791f12ea422bc216390b90
skimlik <kimlik.s@gmail.com>
2016-10-13 17:21:34 +0300

    init proj structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DemoComponent} from './demo/demo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/demo', pathMatch: 'full' },
+  { path: 'demo', component: DemoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
   providers: []
 })
-export class D3ChartsRoutingModule { }
+export class RoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
+import {RoutingModule} from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
